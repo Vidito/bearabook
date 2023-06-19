9930635052aee16f2af58e6a116a3461703ee63c
Vahid <56190096+Vidito@users.noreply.github.com>
2023-06-19 13:30:02 +0200

    Update settings.py

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xx71tbsxfui&p_b+9y*sd*gk1l3u7ry$vh0zzmbcg*1^w8ixm1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
