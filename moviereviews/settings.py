8831181974b4c22ace571afd2abd2d60d78b1d0f
vidito <niamadpour@gmail.com>
2023-06-20 09:24:08 +0200

    add ignore and requirements

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -12,21 +12,24 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 import os.path
 from pathlib import Path
+from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '------------'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
@@ -81,8 +84,9 @@ WSGI_APPLICATION = 'moviereviews.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
     }
 }
 
